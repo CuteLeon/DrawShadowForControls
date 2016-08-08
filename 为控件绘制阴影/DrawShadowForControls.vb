2ccc55d1405dca1e7f423c98c4970ee688b3d3e3
Leon <xiaoyankeji@hotmail.com>
2016-08-08 16:51:22 +0800

    第一次提交

---
+++ 为控件绘制阴影/DrawShadowForControls.vb
@@ -0,0 +1,85 @@
+﻿Module DrawShadowForControls
+    '初始的过程参数
+    Public ShadowSize As Integer = 5                     '阴影大小
+    Public ShadowDistance As Integer = 15             '阴影距离
+    Public ShadowAngle As Integer = 120               '阴影角度
+    Public ShadowColor As Color = Color.Black       '阴影颜色
+    Public ShadowOpacity As Single = 0.75             '阴影不透明度
+
+    '窗体调用
+    Public Sub ShadowMe(ByVal ShadowWindow As Form)
+        Dim ShadowBitmap As New Bitmap(ShadowWindow.Width, ShadowWindow.Height)
+        Dim ShadowLeft, ShadowTop As Integer
+
+        '把所有控件的阴影合并到ShadownBitmap
+        For Each MyControl In ShadowWindow.Controls
+            ShadowLeft = MyControl.Left - ShadowSize - Math.Cos(2 * Math.PI * ShadowAngle / 360) * ShadowDistance
+            ShadowTop = MyControl.Top - ShadowSize + Math.Sin(2 * Math.PI * ShadowAngle / 360) * ShadowDistance
+            CombineBitmap(ShadowBitmap, CreatShadow(MyControl, ShadowSize, ShadowColor), ShadowLeft, ShadowTop)
+        Next
+
+        '把ShadowBitmap绘制到窗体背景
+        Dim MyGraphics As Graphics = Graphics.FromImage(ShadowWindow.BackgroundImage)
+        MyGraphics.DrawImage(ShadowBitmap, 0, 0)
+    End Sub
+
+    '创建阴影位图
+    Private Function CreatShadow(ByVal ShadowControl As Control, ByVal ShadowSize As Integer, ByVal ShadowColor As Color, Optional ByVal ShadowOpacity As Single = 0.75) As Bitmap
+        Dim DoubleSize As Integer = ShadowSize * 2
+        Dim ShadowBitmap As New Bitmap(ShadowControl.Width + DoubleSize, ShadowControl.Height + DoubleSize)
+        Dim ShadowX, ShadowY As Integer
+        Dim Distance As Integer
+        Dim RightBorder = (ShadowBitmap.Width - 1) \ 2
+        Dim DownBorder = (ShadowBitmap.Height - 1) \ 2 '
+        Dim ForX As Integer = DoubleSize, ForY As Integer = DoubleSize
+
+        If DoubleSize > DownBorder Then ForY = DownBorder
+        If DoubleSize > RightBorder Then ForX = RightBorder
+
+        For ShadowX = DoubleSize To RightBorder
+            For ShadowY = 0 To ForY
+                ShadowBitmap.SetPixel(ShadowX, ShadowY, Color.FromArgb(255 * ShadowOpacity * ShadowY ^ 2 / DoubleSize ^ 2, ShadowColor))
+                ShadowBitmap.SetPixel(ShadowBitmap.Width - ShadowX - 1, ShadowY, Color.FromArgb(255 * ShadowOpacity * ShadowY ^ 2 / DoubleSize ^ 2, ShadowColor))
+                ShadowBitmap.SetPixel(ShadowX, ShadowBitmap.Height - ShadowY - 1, Color.FromArgb(255 * ShadowOpacity * ShadowY ^ 2 / DoubleSize ^ 2, ShadowColor))
+                ShadowBitmap.SetPixel(ShadowBitmap.Width - ShadowX - 1, ShadowBitmap.Height - ShadowY - 1, Color.FromArgb(255 * ShadowOpacity * ShadowY ^ 2 / DoubleSize ^ 2, ShadowColor))
+            Next
+        Next
+
+        For ShadowX = 0 To ForX
+            For ShadowY = DoubleSize To DownBorder
+                ShadowBitmap.SetPixel(ShadowX, ShadowY, Color.FromArgb(255 * ShadowOpacity * ShadowX ^ 2 / DoubleSize ^ 2, ShadowColor))
+                ShadowBitmap.SetPixel(ShadowBitmap.Width - ShadowX - 1, ShadowY, Color.FromArgb(255 * ShadowOpacity * ShadowX ^ 2 / DoubleSize ^ 2, ShadowColor))
+                ShadowBitmap.SetPixel(ShadowX, ShadowBitmap.Height - ShadowY - 1, Color.FromArgb(255 * ShadowOpacity * ShadowX ^ 2 / DoubleSize ^ 2, ShadowColor))
+                ShadowBitmap.SetPixel(ShadowBitmap.Width - ShadowX - 1, ShadowBitmap.Height - ShadowY - 1, Color.FromArgb(255 * ShadowOpacity * ShadowX ^ 2 / DoubleSize ^ 2, ShadowColor))
+            Next
+        Next
+
+        For ShadowX = 0 To ForX
+            For ShadowY = 0 To ForY
+                Distance = DoubleSize - Math.Round(Math.Sqrt((DoubleSize - ShadowX) ^ 2 + (DoubleSize - ShadowY) ^ 2))
+                If Distance > -1 Then
+                    ShadowBitmap.SetPixel(ShadowX, ShadowY, Color.FromArgb(255 * ShadowOpacity * Distance ^ 2 / DoubleSize ^ 2, ShadowColor)) '左上
+                    ShadowBitmap.SetPixel(ShadowBitmap.Width - ShadowX - 1, ShadowY, Color.FromArgb(255 * ShadowOpacity * Distance ^ 2 / DoubleSize ^ 2, ShadowColor)) '右上
+                    ShadowBitmap.SetPixel(ShadowX, ShadowBitmap.Height - ShadowY - 1, Color.FromArgb(255 * ShadowOpacity * Distance ^ 2 / DoubleSize ^ 2, ShadowColor)) '左下
+                    ShadowBitmap.SetPixel(ShadowBitmap.Width - ShadowX - 1, ShadowBitmap.Height - ShadowY - 1, Color.FromArgb(255 * ShadowOpacity * Distance ^ 2 / DoubleSize ^ 2, ShadowColor)) '右下
+                End If
+            Next
+        Next
+
+        If Not (ForX = RightBorder Or ForY = DownBorder) Then
+            For ShadowX = DoubleSize To ShadowBitmap.Width - DoubleSize - 1
+                For ShadowY = DoubleSize To ShadowBitmap.Height - DoubleSize - 1
+                    ShadowBitmap.SetPixel(ShadowX, ShadowY, Color.FromArgb(ShadowColor.A * ShadowOpacity, ShadowColor))
+                Next
+            Next
+        End If
+
+        Return ShadowBitmap
+    End Function
+
+    '复制单个控件的ShadownBitmap到整个窗体的ShadowBitmap里
+    Private Sub CombineBitmap(ByVal PaneBitmap As Bitmap, ByVal CopyBitmap As Bitmap, ByVal BitmapX As Integer, ByVal BitmapY As Integer)
+        Dim MyGraphics As Graphics = Graphics.FromImage(PaneBitmap)
+        MyGraphics.DrawImage(CopyBitmap, BitmapX, BitmapY)
+    End Sub
+End Module
---
+++ 为控件绘制阴影/Form1.Designer.vb
@@ -0,0 +1,172 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 重写 Dispose，以清理组件列表。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows 窗体设计器所必需的
+    Private components As System.ComponentModel.IContainer
+
+    '注意:  以下过程是 Windows 窗体设计器所必需的
+    '可以使用 Windows 窗体设计器修改它。  
+    '不要使用代码编辑器修改它。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.RadioButton1 = New System.Windows.Forms.RadioButton()
+        Me.ProgressBar2 = New System.Windows.Forms.ProgressBar()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.CheckBox2 = New System.Windows.Forms.CheckBox()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(12, 12)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(58, 38)
+        Me.TextBox1.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Label1.Location = New System.Drawing.Point(196, 42)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(102, 57)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Label1"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(87, 150)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(102, 57)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Location = New System.Drawing.Point(87, 12)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(58, 38)
+        Me.ProgressBar1.TabIndex = 3
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Location = New System.Drawing.Point(24, 97)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(78, 16)
+        Me.CheckBox1.TabIndex = 4
+        Me.CheckBox1.Text = "CheckBox1"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'RadioButton1
+        '
+        Me.RadioButton1.AutoSize = True
+        Me.RadioButton1.Location = New System.Drawing.Point(24, 128)
+        Me.RadioButton1.Name = "RadioButton1"
+        Me.RadioButton1.Size = New System.Drawing.Size(95, 16)
+        Me.RadioButton1.TabIndex = 5
+        Me.RadioButton1.TabStop = True
+        Me.RadioButton1.Text = "RadioButton1"
+        Me.RadioButton1.UseVisualStyleBackColor = True
+        '
+        'ProgressBar2
+        '
+        Me.ProgressBar2.Location = New System.Drawing.Point(132, 102)
+        Me.ProgressBar2.Name = "ProgressBar2"
+        Me.ProgressBar2.Size = New System.Drawing.Size(100, 23)
+        Me.ProgressBar2.TabIndex = 6
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.ItemHeight = 12
+        Me.ListBox1.Location = New System.Drawing.Point(272, 119)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(120, 88)
+        Me.ListBox1.TabIndex = 7
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(196, 16)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 8
+        Me.Button2.Text = "Button2"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'CheckBox2
+        '
+        Me.CheckBox2.AutoSize = True
+        Me.CheckBox2.Location = New System.Drawing.Point(304, 42)
+        Me.CheckBox2.Name = "CheckBox2"
+        Me.CheckBox2.Size = New System.Drawing.Size(78, 16)
+        Me.CheckBox2.TabIndex = 9
+        Me.CheckBox2.Text = "CheckBox2"
+        Me.CheckBox2.UseVisualStyleBackColor = True
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(24, 56)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(121, 20)
+        Me.ComboBox1.TabIndex = 10
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), System.Drawing.Image)
+        Me.ClientSize = New System.Drawing.Size(404, 241)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.CheckBox2)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.ProgressBar2)
+        Me.Controls.Add(Me.RadioButton1)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents ProgressBar1 As System.Windows.Forms.ProgressBar
+    Friend WithEvents CheckBox1 As System.Windows.Forms.CheckBox
+    Friend WithEvents RadioButton1 As System.Windows.Forms.RadioButton
+    Friend WithEvents ProgressBar2 As System.Windows.Forms.ProgressBar
+    Friend WithEvents ListBox1 As System.Windows.Forms.ListBox
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents CheckBox2 As System.Windows.Forms.CheckBox
+    Friend WithEvents ComboBox1 As System.Windows.Forms.ComboBox
+
+End Class
